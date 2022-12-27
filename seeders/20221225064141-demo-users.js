20eb6bf2577c1aa1a7e8b7b6e7638acc63b29bf6
Chonnikan <chonnikan.aue@gmail.com>
2022-12-27 11:39:12 +0700

    encrypt password in seeder

--- seeders/20221225064141-demo-users.js
+++ seeders/20221225064141-demo-users.js
@@ -8,15 +8,15 @@ module.exports = {
       [
         {
           username: "Mai",
-          password: "1111",
+          password: "$2a$10$DR6jl3ZYqNbxmrDCPVveGe6rT1NS5xsZ0HJk9OAVJJdGep4egHdpG",
         },
         {
           username: "O",
-          password: "2222",
+          password: "$2a$10$ci4H859Vcq7VpvKQ9d79xeEqLyK6PYxtUrE9UlM/aNdqLuR7u8YHC",
         },
         {
           username: "Non",
-          password: "3333",
+          password: "$2a$10$BPcjl2cLXFwT25H7T/gx/Oid0rRkCP2AK1KshUd8gcV7dWMMVW1wa",
         },
       ],
       {}
